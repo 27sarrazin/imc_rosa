c559d5ffbfc6f0bd994560df514d3e76ffd735fd
27sarrazin <sarrazinr5@gmail.com>
2023-01-23 20:29:56 -0400

    update js 23/01/23
    duas linhas de console.log retiradas

--- js/scrip.js
+++ js/scrip.js
@@ -4,8 +4,10 @@
 function calcular(){
     var h=parseFloat(altura.value)
     var p=parseFloat(peso.value)
-    console.log(p/(h*h))
-    var imc=(p/(h*h))
+
+    console.log(h)
+    console.log(p)
+    var imc=p/(h*h)
     var resultado=document.getElementById("resultado")
     resultado.innerHTML=imc.toFixed(2)
     
